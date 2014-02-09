cbef8dc20cc0e51488286ef148b2f5e24fab0637
Der Eckes <markus@erlmann.org>
2014-02-09 11:15:29 +0100

    setting cookies

--- Gemfile
+++ Gemfile
@@ -1,5 +1,6 @@
 source 'https://rubygems.org'
-ruby '2.1.0'
+ruby '1.9.3'
 
 gem 'sinatra', '~>1.4.4'
+gem 'sinatra-contrib'
 gem 'thin'
--- web.rb
+++ web.rb
@@ -1,5 +1,29 @@
 require 'sinatra'
+require "sinatra/cookies"
+require "base64"
 
 get '/' do
-  "Hello, world"
+  "openval: #{cookies[:openval]}" +
+  "closeval: #{cookies[:closeval]}"
+end
+
+get '/get' do
+    "open: #{cookies[:window_open]}"
+    "close: #{cookies[:window_close]}"
+end
+
+get '/set' do
+    now = DateTime.now.to_time
+    open =  now + 1 * 60 * 60 # 1 hour in seconds
+    close = open + 10 * 60    # 10 minutes in seconds
+    open_s = open.to_s
+    close_s = (open + 10 * 60).to_s
+    open_64 = Base64.encode64(open_s)
+    close_64 = Base64.encode64(close_s)
+    "Now: #{now.to_s}<br>" +
+    "Open: #{open.to_s} &rarr; #{open_64}<br>" +
+    "Close: #{close.to_s} &rarr; #{close_64}"
+    cookies[:openval] = open_64
+    cookies[:closeval] = close_64
+    redirect to('/')
 end
