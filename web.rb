e4776873de24a05c0da54a7fd0b1fc855d0174e0
Der Eckes <markus@erlmann.org>
2014-02-10 23:11:11 +0100

    remove time diff calc

--- web.rb
+++ web.rb
@@ -4,25 +4,6 @@ require "sinatra/cookies"
 require "base64"
 require "htmlentities"
 
-class Numeric
-  def duration
-    secs  = self.to_int
-    mins  = secs / 60
-    hours = mins / 60
-    days  = hours / 24
-
-    if days > 0
-      "#{days} days and #{hours % 24} hours"
-    elsif hours > 0
-      "#{hours} hours and #{mins % 60} minutes"
-    elsif mins > 0
-      "#{mins} minutes and #{secs % 60} seconds"
-    elsif secs >= 0
-      "#{secs} seconds"
-    end
-  end
-end
-
 get '/' do
     erb :start
 end
