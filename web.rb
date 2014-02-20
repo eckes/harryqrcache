b7c30bd5b904d54f1b0e958a17465e585bf8867b
Der Eckes <markus@erlmann.org>
2014-02-20 23:47:43 +0100

    final polish

--- web.rb
+++ web.rb
@@ -12,6 +12,9 @@ require 'omniauth-openid'
 require 'openid'
 require 'openid/store/filesystem'
 
+# ==========================================================================================================================
+# === Database Setup =======================================================================================================
+# ==========================================================================================================================
 DataMapper.setup(:default, ENV['DATABASE_URL'] || "postgres://#{ENV['DB_USER']}:#{ENV['DB_PASSWORD']}@localhost/harryqrcache.db")
 class Setting 
 include DataMapper::Resource
@@ -22,7 +25,9 @@ include DataMapper::Resource
 end
 DataMapper.finalize
 
-
+# ==========================================================================================================================
+# === Authentication with OpenID ===========================================================================================
+# ==========================================================================================================================
 use Rack::Session::Cookie, :secret => 'supers3cr3t'
 
 use OmniAuth::Builder do
@@ -47,6 +52,9 @@ OmniAuth.config.on_failure = Proc.new { |env|
   OmniAuth::FailureEndpoint.new(env).redirect_to_failure
 }
 
+# ==========================================================================================================================
+# === Main Logic: Starting, Ending the cache ===============================================================================
+# ==========================================================================================================================
 get '/' do
   slim :start
 end
@@ -56,19 +64,22 @@ get '/startcache' do
 end
 
 get '/endcache' do
-  WINDOW_CLOSE_DELAY = eval(ENV['WINDOW_CLOSE_DELAY'] || Setting.first(:name => "WindowCloseDelay").value)
-  FINAL_TARGET_LATLON= ENV['FINAL_TARGET_LATLON']|| Setting.first(:name => "FinalTargetLatLon").value
+  WINDOW_CLOSE_DELAY = eval(ENV['WINDOW_CLOSE_DELAY'] || Setting.first(:name => "WindowCloseDelay").value) unless defined? WINDOW_CLOSE_DELAY
+  FINAL_TARGET_LATLON= ENV['FINAL_TARGET_LATLON']|| Setting.first(:name => "FinalTargetLatLon").value unless defined? FINAL_TARGET_LATLON
+  unless cookies && cookies[:openval]
+    halt 412, '<a href="/">No cookie found, start again!</a>'
+  end
   open_64 = cookies[:openval]
   open_s  = Base64.decode64(open_64)
   open    = DateTime.parse(open_s)
   close   = open.to_time + WINDOW_CLOSE_DELAY
 
   now = DateTime.now.to_datetime
-  @now_s = now.strftime('%H:%M:%S')
 
-  @open_s = open.strftime('%H:%M:%S')
-  @close_s = close.strftime('%H:%M:%S')
-  @coords = FINAL_TARGET_LATLON
+  @now_s    = now.strftime('%H:%M:%S')
+  @open_s   = open.strftime('%H:%M:%S')
+  @close_s  = close.strftime('%H:%M:%S')
+  @coords   = FINAL_TARGET_LATLON
 
   if now.to_datetime < open.to_datetime
     slim :notyetdue
@@ -80,30 +91,38 @@ get '/endcache' do
 end
 
 post '/startcache' do
-  WINDOW_OPEN_DELAY  = eval(ENV['WINDOW_OPEN_DELAY']  || Setting.first(:name => "WindowOpenDelay").value)
-  NEXT_TARGET_LATLON = ENV['NEXT_TARGET_LATLON'] || Setting.first(:name => "NextTargetLatLon").value
+  WINDOW_OPEN_DELAY  = eval(ENV['WINDOW_OPEN_DELAY']  || Setting.first(:name => "WindowOpenDelay").value) unless defined? WINDOW_OPEN_DELAY
+  NEXT_TARGET_LATLON = ENV['NEXT_TARGET_LATLON'] || Setting.first(:name => "NextTargetLatLon").value unless defined? NEXT_TARGET_LATLON
   now = DateTime.now.to_time
   open    = (now + WINDOW_OPEN_DELAY).to_datetime
   cookies[:openval] = Base64.encode64(open.to_s)
+  redirect '/cachestarted'
+end
+
+get '/cachestarted' do
+  slim :startfailed unless cookies && cookies[:openval]
+  redirect '/startcache' unless defined? NEXT_TARGET_LATLON
   @coords = NEXT_TARGET_LATLON
   slim :startresponse
 end
 
-get '/private/settings' do
-  ADMINS             = ENV['ADMINS']             || Setting.first(:name => "Admins").value
+# ==========================================================================================================================
+# === PRIVATE URLS: Manage settings ========================================================================================
+# ==========================================================================================================================
+before '/private/*' do
+  ADMINS = (ENV['ADMINS'] || Setting.first(:name => "Admins").value) unless defined? ADMINS
   unless session && session[:email] && (ADMINS.include? session[:email])
     halt 401, '<a href="/auth/openid">authentication required</a>'
   end
+end
+
+get '/private/settings' do
   @user_name = (session[:email] == 'meliundeckes@gmail.com' && 'Eckes') || 'Harry'
   @settings = Setting.all
   slim :showsettings
 end
 
 post '/private/settings' do
-  ADMINS             = ENV['ADMINS']             || Setting.first(:name => "Admins").value
-  unless session && session[:email] && (ADMINS.include? session[:email])
-    halt 401, '<a href="/auth/openid">authentication required</a>'
-  end
   params.each do |key, value|
     Setting.get(key).update(:value => value)
   end
@@ -111,26 +130,22 @@ post '/private/settings' do
 end
 
 get '/private/addsetting' do
-  ADMINS             = ENV['ADMINS']             || Setting.first(:name => "Admins").value
-  unless session && session[:email] && (ADMINS.include? session[:email])
-    halt 401, '<a href="/auth/openid">authentication required</a>'
-  end
   slim :addsetting
 end
 
 post '/private/addsetting' do
-  ADMINS             = ENV['ADMINS']             || Setting.first(:name => "Admins").value
-  unless session && session[:email] && (ADMINS.include? session[:email])
-    halt 401, '<a href="/auth/openid">authentication required</a>'
-  end
   Setting.create( name: params['setting_name'], value: params['setting_value'], description: params['setting_description'] )
   redirect '/private/settings'
 end
 
 __END__
 
+# ==========================================================================================================================
+# === Web Page Templates ===================================================================================================
+# ==========================================================================================================================
+
 @@ layout
-doctype html
+doctype 5
 html
 head
 meta charset="utf-8"
@@ -148,6 +163,12 @@ ul
 h2 Alles Klar?
 form method="POST" action="startcache"
   input.button type="submit" value="STARTEN"
+
+@@ startfailed
+h1.error Start fehlgeschlagen
+p Leider ist irgend etwas schief gegangen. Bitte
+a href="/" nochmal probieren
+p Cookies müssen aktiviert sein!
 
 @@ startresponse
 h1 Der Timer wurde gestartet
@@ -186,7 +207,9 @@ form action="/private/settings" method="POST"
           label #{setting.description}
   input.button type="submit" value="Update Settings"
 hr
-a href='/private/addsetting' Add Setting
+a href='/private/addsetting' Add a setting |
+a href='/private/settings' Show settings |
+a href='/' Start Page
 
 @@addsetting
 h1 Add setting
@@ -208,15 +231,10 @@ form action="/private/addsetting" method="POST"
       td
         input id="setting_description" type="text" name="setting_description" value=""
   input.button type="submit" value="Add Setting"
+hr
+a href='/private/addsetting' Add a setting |
+a href='/private/settings' Show settings |
+a href='/' Start Page
 
-@@ listsettings
-h1 Settings
-table
-  - @params.each do |param|
-    tr
-      td
-        "#{param.name}"
-      td
-        "#{param.value}"
 
 / vim: set sw=2 ts=2 enc=utf8:
